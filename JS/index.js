99a690160e4af3c264e3e5503378ec9abd04439c
habibouomar <habibou.omar@gmail.com>
2022-02-18 17:29:09 +0100

    add code index.js

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,14 @@ setInterval(function() {
     timer++;
     spanElt.textContent = timer
 }, 1000);
+
+let spanElt = document.querySelector(".timer span");
+let timer = 30;
+
+let timer_id = setInterval(function() {
+timer--;
+spanElt.textContent = timer;
+if (timer < 1) {
+clearInterval(timer_id);
+}
+}, 1000);
